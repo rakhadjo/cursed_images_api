6ed03f3183173cd8b713da96257926b040730fae
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-07-21 11:38:24 +0700

    initialized project

---
+++ cursed/src/main/java/com/cursed/cursed/CursedApplication.java
@@ -0,0 +1,13 @@
+package com.cursed.cursed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursedApplication.class, args);
+	}
+
+}
---
+++ cursed/src/test/java/com/cursed/cursed/CursedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursed.cursed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
