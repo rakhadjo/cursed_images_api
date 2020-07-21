ceb05310dd484b67e61e70b435e8701a118a0942
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-07-22 00:46:44 +0700

    added json-type conversion for image type

--- cursed/src/main/java/com/cursed/cursed/models/Imej.java
+++ cursed/src/main/java/com/cursed/cursed/models/Imej.java
@@ -18,9 +18,18 @@ public class Imej {
     @Id
     private ObjectId _id;
     private String url;
+    private int num;
     
     public Imej() {}
     
     public String getUrl() { return this.url; }
     public ObjectId getId() { return this._id; }
+    public int getNum() { return this.num; }
+    
+    public org.bson.Document toJSON() {
+        return new org.bson.Document()
+                .append("_id", this._id)
+                .append("num", this.num)
+                .append("url", this.url);
+    }
 }
--- cursed/src/main/java/com/cursed/cursed/repositories/ImejRepo.java
+++ cursed/src/main/java/com/cursed/cursed/repositories/ImejRepo.java
@@ -8,6 +8,7 @@ package com.cursed.cursed.repositories;
 import com.cursed.cursed.models.Imej;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
@@ -19,7 +20,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ImejRepo extends MongoRepository<Imej, String>{
     
-    Imej findById();
+    Imej findByNum(@Param("num") int num);
     
     List<Imej> findAll();
     
