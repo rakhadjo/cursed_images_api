8163647e306e99b5c0779d58103d4659742331e2
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-07-22 00:47:45 +0700

    api can save images and retrieve random images from within

--- cursed/src/main/java/com/cursed/cursed/control/CursedControl.java
+++ cursed/src/main/java/com/cursed/cursed/control/CursedControl.java
@@ -5,10 +5,15 @@
  */
 package com.cursed.cursed.control;
 
+import com.cursed.cursed.models.Imej;
 import com.cursed.cursed.repositories.ImejRepo;
+import java.util.Random;
+import javax.validation.Valid;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,12 +27,31 @@ public class CursedControl {
     
     @Autowired
     private ImejRepo repo;
+    //private Document entries = new Document("code", repo.findAll());
+    private Random rand = new Random();
     
     @GetMapping("/test")
     public Document getTest() {
         return new Document()
                 .append("rc", "00")
                 .append("code", repo.findAll());
+    }
+    
+    @GetMapping("/get")
+    public Document getRandom() {
+        int num = rand.nextInt((int)repo.count());
+        return repo.findByNum(num).toJSON();
+    }
+    
+    @PostMapping("/save")
+    public Document saveImej(@Valid @RequestBody Imej imej) {
+        if (imej.getNum() > (int)repo.count() && repo.findByNum(imej.getNum()) == null) {
+            repo.save(imej);
+            return new Document().append("rc", "00");
+        }
+        return new Document()
+                .append("rc", "10")
+                .append("message", "num already exists");
     }
     
 }
