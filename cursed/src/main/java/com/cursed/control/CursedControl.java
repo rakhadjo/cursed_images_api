7a1d34ef9320065dcad85fd043ef44129ed7ec9f
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-07-21 13:06:34 +0700

    add controller for mvc

---
+++ cursed/src/main/java/com/cursed/control/CursedControl.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cursed.control;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author rakhadjo
+ */
+@RestController
+@RequestMapping("/c")
+public class CursedControl {
+    
+}
