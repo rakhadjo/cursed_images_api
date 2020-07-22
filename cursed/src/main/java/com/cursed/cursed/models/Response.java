2b4ff620d9aec3b0e345ff5756f956c03f701551
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-07-22 14:51:42 +0700

    added response objects

---
+++ cursed/src/main/java/com/cursed/cursed/models/Response.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cursed.cursed.models;
+
+import java.util.List;
+import org.bson.Document;
+
+/**
+ *
+ * @author rakhadjo
+ */
+public class Response {
+    
+    //  Response Code
+    private String rc;
+    //  Message
+    private String message;
+    //  Imej object
+    private Imej imej;
+    //  Imej List
+    private List<Imej> imejs;
+    
+    public Response() {}
+    
+    public Response(Result m) {
+        setCodes(m);
+    }
+    
+    public final void setCodes(Result m) {
+        switch (m) {
+            case SUCCESS:
+                this.rc = "00";
+                this.message = "Success";
+                break;
+            case EXISTS:
+                this.rc = "10";
+                this.message = "Already exists";
+                break;
+            case FAIL_ALL:
+                this.rc = "11";
+                this.message = "Failed to retrieve all";
+            default:
+                this.rc = "99";
+                this.message = "Undefined Error";
+                break;
+        }
+    }
+    
+    public void setImej(Imej i) {
+        this.imej = i;
+    }
+    
+    public void setImejs(List<Imej> l) {
+        this.imejs = l;
+    }
+    
+    public Document toJSON() {
+        Document doc = new Document()
+                .append("rc", this.rc)
+                .append("message", this.message);
+        
+        if (!this.imej.equals(null)) {
+            doc.append("imej", this.imej);
+        }
+        else if (!this.imejs.isEmpty()) {
+            doc.append("imejs", this.imejs);
+        }
+        return doc;
+        
+    }
+    
+}
