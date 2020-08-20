658167055e86f82eb90ad5403cc929732445dc69
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-08-21 00:22:40 +0700

    add: key mysql element

---
+++ cursed/src/main/java/com/cursed/cursed/repositories/Key2Repo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cursed.cursed.repositories;
+
+import com.cursed.cursed.models.Key2;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author rakhadjo
+ */
+public interface Key2Repo extends CrudRepository<Key2, Integer> {
+    
+}
