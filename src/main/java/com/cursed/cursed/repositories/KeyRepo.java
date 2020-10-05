d167e5ee88a3916499041276857baa706fb59080
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-10-05 14:28:08 +0700

    change: findByEmail -> findByApi_key

--- src/main/java/com/cursed/cursed/repositories/KeyRepo.java
+++ src/main/java/com/cursed/cursed/repositories/KeyRepo.java
@@ -22,6 +22,6 @@ public interface KeyRepo extends MongoRepository<Key, String>{
      * @param email
      * @return
      */
-    Key findByEmail(@Param("email") String email);
+    Key findByApi_key(@Param("api_key") String api_key);
     
 }
